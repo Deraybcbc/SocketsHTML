aeeb302f16270976f90c670992363323834039e3
Deraybcbc <a22kevburcac@inspedralbes.cat>
2024-11-15 13:18:45 +0100

    HACERLO MAS BONITO PORQUE SI

--- public/utils.js
+++ public/utils.js
@@ -2,13 +2,14 @@
 const socket = io('http://localhost:3000');
 
 let btnEnviar;
-let messageInput
+let messageInput;
+let screenMessage;
 
 
 function init() {
     btnEnviar = document.querySelector('#btnEnviar');
     messageInput = document.querySelector('#messageInput');
-
+    screenMessage = document.querySelector('#messages');
 }
 
 function enviarMessage() {
@@ -21,6 +22,7 @@ function enviarMessage() {
         console.log("FRONT: ",messageInput.value);
         
         socket.emit('messageHTML', messageInput.value);
+
         messageInput.value = '';
     })
 }
@@ -34,6 +36,16 @@ document.addEventListener('DOMContentLoaded', function () {
     socket.on('connection', () => {
         console.log("conectado al servidor");
     });
+
+    socket.on('messageBack',(msg)=>{
+        console.log("Mensage  llegado Back: ", msg);
+        // Crear un nuevo elemento para el mensaje
+        const newMessage = document.createElement('p');
+        newMessage.textContent = msg;
+
+        // Añadirlo al contenedor de mensajes
+        screenMessage.appendChild(newMessage);
+    })
 
     socket.on('disconnect', () => {
         console.log('Desconectado del servidor');
